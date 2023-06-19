8ee428f706dd605c1e84d8a697c3824db9cde821
glistering96 <glistering96@gmail.com>
2023-06-19 11:27:14 +0900

    customized FCNet final activiation

--- model.py
+++ model.py
@@ -11,8 +11,14 @@ def Gaussian_CDF(x):
 
 
 class FCNet(nn.Module):
-    def __init__(self, in_features, out_features, num_layers=1, hidden_features=100,
-                 activation=nn.ReLU, dropout=0.0):
+    def __init__(self, 
+                 in_features, 
+                 out_features,
+                 num_layers=1,
+                 hidden_features=100,
+                 in_layer_activation=nn.ReLU,
+                 final_layer_activation=nn.ReLU, 
+                 dropout=0.0):
         super(FCNet, self).__init__()
 
         self.layers = nn.ModuleList()
@@ -21,12 +27,12 @@ class FCNet(nn.Module):
         for i in range(num_layers - 1):
             ##num layer 개수만큼 hidden layer 생성
             self.layers.append(nn.Linear(hidden_features, hidden_features))
-            self.layers.append(activation())
+            self.layers.append(in_layer_activation())
             self.layers.append(nn.Dropout(dropout))
 
         self.layers.append(nn.Linear(hidden_features, out_features))
-        ## 마지막 레이어의 activation은 상황에 따라 다르게 설정해야함 (reconstruction 일 때는 RElu, mask 일 때는 sigmoid)
-        self.layers.append(activation())
+        ## 마지막 레이어의 in_layer_activation은 상황에 따라 다르게 설정해야함 (reconstruction 일 때는 RElu, mask 일 때는 sigmoid)
+        self.layers.append(final_layer_activation())
 
     def forward(self, x):
         for layer in self.layers:
@@ -53,11 +59,15 @@ class SEFS_SS_Phase(nn.Module):
         self.fc_activate_fn = network_settings['fc_activate_fn']
 
         self.encoder = FCNet(self.x_dim, self.z_dim, self.num_layers_e, self.h_dim_e,
-                             self.fc_activate_fn)
+                             in_layer_activation=self.fc_activate_fn)
+        
         self.decoder_x = FCNet(self.z_dim, self.x_dim, self.num_layers_d, self.h_dim_d,
-                                self.fc_activate_fn)
+                                in_layer_activation=self.fc_activate_fn)
+        
         self.decoder_m = FCNet(self.z_dim, self.x_dim, self.num_layers_d, self.h_dim_d,
-                                nn.Sigmoid)
+                               in_layer_activation=self.fc_activate_fn,
+                               final_layer_activation=nn.Sigmoid)
+        
         ## out put for mas should be in [0,1] so sigmoid function is used
         
     def sample_gate_vector(self,x):
