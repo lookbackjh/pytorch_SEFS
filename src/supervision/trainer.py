1520e428186f8ee3449c8b942ddd43cc91cfccaf
lookbackjh <jjjjunho1011@gmail.com>
2023-06-22 12:30:59 +0900

    relaxedgate fixed

--- src/supervision/trainer.py
+++ src/supervision/trainer.py
@@ -96,7 +96,7 @@ class STrainer(pl.LightningModule):
         # shape of m: (batch_sizex, x_dim)
  
         # if m is greater than 0.5 want to make it 1
-        m = (m > 0.5).float()
+        #m = (m > 0.5).float()
 
         # generate feature subset
         x_tilde = torch.mul(m, x) + torch.mul(1. - m, self.x_mean)
@@ -105,16 +105,18 @@ class STrainer(pl.LightningModule):
         z = self.model.encoder(x_tilde)
         
         # estimate x_hat from decoder
-        y_hat_logit = self.model.predictor(z).squeeze(1)
+        y_hat_logit = self.model.predictor_linear(z).squeeze(1)
 
         # compute loss
-        loss_y = F.binary_cross_entropy_with_logits(y_hat_logit, y)
+        loss_y = F.binary_cross_entropy_with_logits(y_hat_logit, y,reduction='mean') # loss for y_hat
         total_loss=loss_y+self.beta_coef*pi.sum()
 
         # logging losses
         self.log('loss/total', total_loss, prog_bar=True)
         self.log('loss/temp', loss_y, prog_bar=True)
-        self.log('metric/pi_var', pi.var(), prog_bar=True)  # variance of pi, we expect it to be increasing
+        self.log('metric/pi_var', pi[0], prog_bar=True)  # variance of pi, we expect it to be increasing
+        self.log('metric/pi_1', pi[10], prog_bar=True) 
+        self.log('metric/pi_2', pi[20], prog_bar=True) 
 
         return total_loss
     
