0198bb0e69dda5e0ebbdc6e400d25cb753a47c89
lookbackjh <jjjjunho1011@gmail.com>
2023-06-30 16:52:41 +0900

    earlystop aptience change

--- SEFS_test.py
+++ SEFS_test.py
@@ -112,7 +112,7 @@ def main():
             exp_name=get_log_dir(args), # this is the name of the experiment.
                                         # you can change it to whatever you want using the function above.
                                         
-            early_stopping_patience=10000
+            early_stopping_patience=1000
         )
 
         sefs.train()
