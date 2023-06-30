cd3080663192643de6295afc020b3a7654c30e88
lookbackjh <jjjjunho1011@gmail.com>
2023-06-30 16:42:42 +0900

    experiment name

--- SEFS_test.py
+++ SEFS_test.py
@@ -50,22 +50,14 @@ def get_log_dir(args):
     # cur_time = datetime.now().strftime("%Y%m%d-%H%M%S")
     # exp_name = f'test_{cur_time}'
     
-<<<<<<< HEAD
-    exp_name = f"self_super/l1_coef-{args.l1_coef}"
-=======
     exp_name = f"beta-{args.beta}/no_pi_mean/l1_coef-{args.l1_coef}/logitpi"
->>>>>>> 839d650850c945a04632bdd875ce3708cb8ec104
 
     return exp_name
 
 
 def main():
-<<<<<<< HEAD
-    for _l1_coef in [0.0, 0.1, 0.01, 0.001, 0.0001, 0.00001]:
-=======
     for beta in [0.05]:
         _l1_coef = 1e-5
->>>>>>> 839d650850c945a04632bdd875ce3708cb8ec104
         data = DataWrapper(SyntheticData())
         # val_data = DataWrapper(SyntheticData(456))    # validation is currently not supported for semi-supervision
         val_data = None
